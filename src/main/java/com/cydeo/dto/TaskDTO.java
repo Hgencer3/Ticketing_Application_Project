074100c353ddc28c32e71834df7c26e7b6b41efe
Hgencer3 <onlygencer@gmail.com>
2022-11-27 19:02:00 -0500

    ORM Part 4 Task Controller

--- src/main/java/com/cydeo/dto/TaskDTO.java
+++ src/main/java/com/cydeo/dto/TaskDTO.java
@@ -5,11 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.time.LocalDate;
 import java.util.UUID;
 
 @NoArgsConstructor
-
 @Data
 public class TaskDTO {
 
